2f715bca3ab8c1aa75f5dffeff09d15457665236
DroneBreaker <brownierz01@gmail.com>
2026-01-16 18:22:59 +0000

    created models, and routes for events

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/DroneBreaker/Event-Booking-API.git/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Setup engine
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(c *gin.Context) {
+	events := models.GetEvents()
+
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request payload",
+		})
+
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Event created successfully",
+		"event":   event,
+	})
+}
